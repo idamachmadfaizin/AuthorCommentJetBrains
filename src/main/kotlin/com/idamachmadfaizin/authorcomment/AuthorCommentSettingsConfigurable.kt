3d3f0c0168aa05273701391f418803d2265c838a
Idam Achmad Faizin <idamachmadfaizin@gmail.com>
2024-12-13 22:08:45 +0700

    feat: use FormBuilder on setting configurable.

--- src/main/kotlin/com/idamachmadfaizin/authorcomment/AuthorCommentSettingsConfigurable.kt
+++ src/main/kotlin/com/idamachmadfaizin/authorcomment/AuthorCommentSettingsConfigurable.kt
@@ -1,52 +1,84 @@
 package com.idamachmadfaizin.authorcomment
 
 import com.intellij.openapi.options.Configurable
-import java.awt.GridBagConstraints
+import com.intellij.ui.DocumentAdapter
+import com.intellij.util.ui.FormBuilder
 import java.awt.GridBagLayout
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.swing.JComponent
 import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.JTextField
+import javax.swing.event.DocumentEvent
 
 class AuthorCommentSettingsConfigurable : Configurable {
-    private var panel: JPanel? = null
-    private var authorNameField: JTextField? = null
-    private var dateFormatField: JTextField? = null
+    private var panel: JPanel
+    private var authorNameField: JTextField
+    private var dateFormatField: JTextField
+    private var dateFormatPreview: JLabel
+    private val datePreview = LocalDateTime.of(LocalDate.now().year, 1, 31, 0, 0)
     private val settings = AuthorCommentSettings.getInstance()
+    private val exampleDateFormats = arrayOf("EEEE, MMMM d, yyyy", "yyyy-MM-dd", "MM/dd/yyyy")
+
+    init {
+        panel = JPanel(GridBagLayout())
+        authorNameField = JTextField(settings.authorName)
+        dateFormatField = JTextField(settings.dateFormat)
+        dateFormatField.toolTipText =
+            exampleDateFormats.joinToString(
+                "<br/>",
+                "<html>Example date formats:<br/>",
+                "</html>",
+            ) {
+                "$it (${getDateFormat(it)})"
+            }
+        dateFormatPreview = JLabel(getDateFormat(dateFormatField.text))
+
+        dateFormatField.document.addDocumentListener(object : DocumentAdapter() {
+            override fun textChanged(e: DocumentEvent) {
+                dateFormatPreview.text = getDateFormat(dateFormatField.text)
+            }
+        })
+    }
 
     override fun createComponent(): JComponent {
-        panel = JPanel(GridBagLayout())
-        val c = GridBagConstraints()
+        panel = FormBuilder.createFormBuilder()
+            .addLabeledComponent(JLabel("Author Name:"), authorNameField, 1)
+            .addLabeledComponent(JLabel("Date Format:"), dateFormatField, 1)
+            .addLabeledComponent(JLabel(), dateFormatPreview)
+            .addComponentFillVertically(JPanel(), 0)
+            .panel
 
-        c.fill = GridBagConstraints.HORIZONTAL
-        c.gridx = 0
-        c.gridy = 0
-        panel!!.add(JLabel("Author Name:"), c)
-
-        c.gridx = 1
-        authorNameField = JTextField(settings.authorName)
-        panel!!.add(authorNameField!!, c)
-
-        c.gridx = 0
-        c.gridy = 1
-        panel!!.add(JLabel("Date Format:"), c)
-
-        c.gridx = 1
-        dateFormatField = JTextField(settings.dateFormat)
-        panel!!.add(dateFormatField!!, c)
-
-        return panel!!
+        return panel
     }
 
     override fun isModified(): Boolean {
-        return authorNameField?.text != settings.authorName ||
-                dateFormatField?.text != settings.dateFormat
+        return authorNameField.text != settings.authorName ||
+                dateFormatField.text != settings.dateFormat
     }
 
     override fun apply() {
-        settings.authorName = authorNameField?.text ?: "Author Name"
-        settings.dateFormat = dateFormatField?.text ?: "EEEE, MMMM d, yyyy"
+        settings.authorName = authorNameField.text
+        settings.dateFormat = dateFormatField.text
+    }
+
+    override fun reset() {
+        if (!isModified) return
+
+        authorNameField.text = settings.authorName
+        dateFormatField.text = settings.dateFormat
     }
 
     override fun getDisplayName(): String = "Author Comment Settings"
+
+    private fun getDateFormat(pattern: String): String {
+        try {
+            val dateFormatter = DateTimeFormatter.ofPattern(pattern)
+            return datePreview.format(dateFormatter)
+        } catch (e: Exception) {
+            return "Error: ${e.message}"
+        }
+    }
 }
