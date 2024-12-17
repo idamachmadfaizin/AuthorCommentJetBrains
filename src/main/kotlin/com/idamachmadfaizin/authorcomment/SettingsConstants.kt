67fe9958280fbcb778d2cb8303e4c7817e6c00ce
Idam Achmad Faizin <idamachmadfaizin@gmail.com>
2024-12-18 00:02:53 +0700

    feat: create SettingsConstants.

---
+++ src/main/kotlin/com/idamachmadfaizin/authorcomment/SettingsConstants.kt
@@ -0,0 +1,9 @@
+package com.idamachmadfaizin.authorcomment
+
+import java.time.LocalDate
+
+class SettingsConstants {
+    companion object {
+        val DATE_PREVIEW: LocalDate = LocalDate.of(LocalDate.now().year, 1, 31)
+    }
+}
